ffc13ef24153199becbf33a29a23f2538811e157
Asadullin Rem <rem.asadullin@ccsteam.ru>
2022-07-30 03:36:02 +0500

    commit message

--- helpers/tagsHelpers.js
+++ helpers/tagsHelpers.js
@@ -1,4 +1,14 @@
-const getTagsArray = caption => {
-    const items = caption.split(' ')
-    return items
+module.exports = {
+    getList: caption => {
+        const items = caption.split(' ')
+        console.log(caption);
+        console.log(items);
+        const tags = items.map(item => {
+            if ([' ', '#', ','].contains(item[0])){
+              return item.substring(1)
+            }
+            return item
+        })
+        return tags
+    }
 }
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const {mongoURL, channel_id} = require('./config/default.json')
 const User = require('./models/Users')
 const Meme = require('./models/Memes')
+const {getList} = require("./helpers/tagsHelpers");
+const { getTagsList } = require('./helpers/tagsHelpers')
 
 
 bot.setMyCommands([
@@ -18,7 +20,7 @@ const saveMemeToDatabase = () => {
 
 }
 
-const  start = async () => {
+const start = async () => {
     try {
         await mongoose.connect(mongoURL, {
             useNewUrlParser: true,
@@ -29,60 +31,56 @@ const  start = async () => {
         console.log(e);
     }
 
-    bot.on('message', async msg => {
+    bot.on('message',  async msg => {
         const {
             text,
             photo,
             caption,
             chat: { id: chatId}
             } = msg
-        console.log(msg);
         try {
-            if (msg.text){
-                switch (text) {
-                    case '/start':
-                        await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/3.webp')
-                        await bot.sendMessage(chatId, 'Добро пожаловать!')
-                        const ifUserExists = await User.exists({chatId})
-                        if (!ifUserExists) {
-                            const user = await User.findOne({chatId})
-                            await user.save()
-                        }
+           if (photo) {
+               console.log(photo);
+               console.log(caption);
+               const fileId = photo[0].file_id
 
-                        break
-                }
-            }
-           if (msg.photo) {
-                await bot.sendPhoto(channel_id, msg.photo[2].file_id)
-                if (msg.caption){
-                    const tagsList = getTagsArray(msg.caption)
-                }
-                bot.on('channel_post', async post => {
+               if (fileId) {
+                   await bot.sendPhoto(channel_id, fileId)
+               }
+               bot.on('channel_post',  post => {
+                   console.log('channel_post');
                    console.log(post);
                })
+               const list = getList(caption)
+               console.log(list);
            }
+
+        // >>2297508
+        //     Вдруг мне повезло и тут кто-то сразу кто-то увидит ошибку.
+        //
+        //         Пикрелейтед, хочу реализовать следующее:
+        //         - Отправляю боту картинку, он берет из сообщения photo и caption
+        //     - Картинку с помощью её  fileId постит в канал
+        //     - Этот же самый канал слушает  и когда в нем появляется пост,  берет из него что-нибудь,  вообще мне нужен id поста.
+        //
+            if (text){
+                // console.log(text);
+                // switch (text) {
+                //     case '/start':
+                //         await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/3.webp')
+                //         await bot.sendMessage(chatId, 'Добро пожаловать!')
+                //         const ifUserExists = await User.exists({chatId})
+                //         if (!ifUserExists) {
+                //             const user = await User.findOne({chatId})
+                //             await user.save()
+                //         }
+                //         break
+                // }
+            }
         } catch (e) {
             return bot.sendMessage(chatId, e)
         }
     })
-    bot.on('callback_query', async msg => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-        const user = await User.findOne({chatId})
-        const num = game_numbers[chatId].toString()
-        switch (data) {
-            case '/again':
-                return await startGame(chatId)
-            case num:
-                user.right += 1
-                user.save()
-                return  bot.sendMessage(chatId,'Угадал!',  replayOptions)
 
-            default:
-                user.wrong += 1
-                user.save()
-                return bot.sendMessage(chatId,`Нет, число было ${num}`,  replayOptions)
-        }
-    })
 }
 start()
